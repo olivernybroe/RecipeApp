5c9aa03c4547c353121e5648b719a0706e2ba0c6
Oliver Nybroe <olivernybroe@gmail.com>
2018-10-28 17:34:28 +0100

    Feat: added sample recipe from firebase

--- lib/Models/Recipe.dart
+++ lib/Models/Recipe.dart
@@ -1,4 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Recipe {
+    final String name;
+    final DocumentReference reference;
 
+    Recipe.fromMap(Map<String, dynamic> map, {this.reference})
+        : assert(map['name'] != null), name = map['name'];
+
+    Recipe.fromSnapshot(DocumentSnapshot snapshot)
+        : this.fromMap(snapshot.data, reference: snapshot.reference);
+
+    @override
+    String toString() => "Recipe<$name>";
 }
--- lib/pages/RecipesPage.dart
+++ lib/pages/RecipesPage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:MealEngineer/Models/Recipe.dart';
+
 
 
 class RecipesPage extends StatefulWidget {
@@ -15,24 +18,49 @@ class _RecipeState extends State<RecipesPage> {
     @override
     Widget build(BuildContext context) {
         return Scaffold(
-            body: Card(
-                child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                        const ListTile(
-                            leading: Icon(Icons.album),
-                            title: Text('Title'),
-                            subtitle: Text(
-                                'A really nice subtitle'),
-                        ),
-                    ],
-                ),
-            ),
+            body: _buildBody(context),
 
             floatingActionButton: new FloatingActionButton(
                 onPressed: null,
                 tooltip: 'Create a recipe',
                 child: new Icon(Icons.add),
+            ),
+        );
+    }
+
+
+    Widget _buildBody(BuildContext context) {
+        return StreamBuilder<QuerySnapshot>(
+            stream: Firestore.instance.collection('users').document('WnUSOQjMQbfxS0dsCobG').collection('recipes').snapshots(),
+            builder: (context, snapshot) {
+                if (!snapshot.hasData) return LinearProgressIndicator();
+
+                return _buildList(context, snapshot.data.documents);
+            },
+        );
+    }
+
+    Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
+        return ListView(
+            padding: const EdgeInsets.only(top: 20.0),
+            children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+        );
+    }
+
+    Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
+        final recipe = Recipe.fromSnapshot(data);
+
+        return Card(
+            child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                    ListTile(
+                        leading: Icon(Icons.album),
+                        title: Text(recipe.name),
+                        subtitle: Text(
+                            'A really nice subtitle'),
+                    ),
+                ],
             ),
         );
     }
