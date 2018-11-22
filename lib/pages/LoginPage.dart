7acd90f6ced9025b92b8d8e1c58c292947e87e43
Oliver Nybroe <olivernybroe@gmail.com>
2018-11-22 14:45:57 +0100

    Fix: Fixed login

--- lib/pages/LoginPage.dart
+++ lib/pages/LoginPage.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:MealEngineer/Models/Recipe.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 
 
 
@@ -19,27 +18,17 @@ class LoginPage extends StatefulWidget {
 class _LoginState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-
-
+    return Scaffold(
       body: Center(
-
-
-        child: new Column(
+        child: Column(
           children: <Widget>[
-
-            Text(
-              'Recipe App',
-              style: TextStyle(decoration: TextDecoration.underline),
-            ),
-
-            new RaisedButton(
-                child: new Text('Login with Google'),
+            SignInButton(
+                Buttons.Google,
                 onPressed: () {
-                  _handleSignIn()
-                      .then((FirebaseUser user) => print(user))
-                      .catchError((e) => print(e));
-                }
+                    _handleSignIn()
+                        .then((FirebaseUser user) => print(user))
+                        .catchError((e) => print(e));
+                },
             ),
           ],
         ),
@@ -47,30 +36,14 @@ class _LoginState extends State<LoginPage> {
     );
   }
 
-
-      /*Container(
-      decoration: BoxDecoration(color: Colors.blue[500]),
-        child: Center(
-          child:
-            new RaisedButton(
-              child: new Text('Login with Google'),
-                onPressed: () {
-              _handleSignIn()
-                  .then((FirebaseUser user) => print(user))
-                  .catchError((e) => print(e));
-            }),
-        )
-    );*/
-
-
   Future<FirebaseUser> _handleSignIn() async {
-    GoogleSignInAccount googleUser = await _googleSignIn.signIn();
-    GoogleSignInAuthentication googleAuth = await googleUser.authentication;
-    FirebaseUser user = await _auth.signInWithGoogle(
-      accessToken: googleAuth.accessToken,
-      idToken: googleAuth.idToken,
-    );
-    print("signed in " + user.displayName);
-    return user;
+      GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+      GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+      FirebaseUser user = await _auth.signInWithGoogle(
+          accessToken: googleAuth.accessToken,
+          idToken: googleAuth.idToken,
+      );
+      print("signed in " + user.displayName);
+      return user;
   }
 }
